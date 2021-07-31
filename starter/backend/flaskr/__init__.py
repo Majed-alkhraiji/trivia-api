96e1147115b4382a9ee5d5f0557b7d513fa5985c
Majed-alkhraiji <mjode2@gmail.com>
2021-07-31 18:56:18 +0300

    finished all todos

--- starter/backend/flaskr/__init__.py
+++ starter/backend/flaskr/__init__.py
@@ -26,14 +26,8 @@ def create_app(test_config=None):
     setup_db(app)
     CORS(app)
 
-    '''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
-  '''
     CORS(app, resources={'/': {'origins': '*'}})
 
-    '''
-  @TODO: Use the after_request decorator to set Access-Control-Allow
-  '''
 
     @app.after_request
     def after_request(response):
@@ -41,11 +35,6 @@ def create_app(test_config=None):
         response.headers.add('Access-Control-Allow-Headers', 'GET, POST, PATCH, DELETE, OPTION')
         return response
 
-    '''
-  @TODO: 
-  Create an endpoint to handle GET requests 
-  for all available categories.
-  '''
 
     @app.route('/categories')
     def get_categories():
@@ -60,18 +49,6 @@ def create_app(test_config=None):
             'categories': result
         })
 
-    '''
-  @TODO: 
-  Create an endpoint to handle GET requests for questions, 
-  including pagination (every 10 questions). 
-  This endpoint should return a list of questions, 
-  number of total questions, current category, categories. 
-
-  TEST: At this point, when you start the application
-  you should see questions and categories generated,
-  ten questions per page and pagination at the bottom of the screen for three pages.
-  Clicking on the page numbers should update the questions. 
-  '''
 
     @app.route('/questions')
     def get_questions():
@@ -94,13 +71,6 @@ def create_app(test_config=None):
             'categories': dict_categories
         })
 
-    '''
-  @TODO: 
-  Create an endpoint to DELETE question using a question ID. 
-
-  TEST: When you click the trash icon next to a question, the question will be removed.
-  This removal will persist in the database and when you refresh the page. 
-  '''
 
     @app.route('/questions/<int:qid>', methods=['DELETE'])
     def delete_question(qid):
@@ -257,42 +227,3 @@ def create_app(test_config=None):
 
     return app
 
-
-'''
-
-TEST: When you submit a question on the "Add" tab, 
-the form will clear and the question will appear at the end of the last page
-of the questions list in the "List" tab.  
-'''
-
-'''
-
-
-TEST: Search by any phrase. The questions list will update to include 
-only question that include that string within their question. 
-Try using the word "title" to start. 
-'''
-
-'''
-TEST: In the "List" tab / main screen, clicking on one of the 
-categories in the left column will cause only questions of that 
-category to be shown. 
-'''
-
-'''
-@TODO: 
-Create a POST endpoint to get questions to play the quiz. 
-This endpoint should take category and previous question parameters 
-and return a random questions within the given category, 
-if provided, and that is not one of the previous questions. 
-
-TEST: In the "Play" tab, after a user selects "All" or a category,
-one question at a time is displayed, the user is allowed to answer
-and shown whether they were correct or not. 
-'''
-
-'''
-@TODO: 
-Create error handlers for all expected errors 
-including 404 and 422. 
-'''
